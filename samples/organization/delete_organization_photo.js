f5d47195fa41f9bda84ad483a99a86874baafa84
raja.k <raja.k@zohocorp.com>
2024-07-04 18:04:00 +0530

    Update sample

--- samples/organization/delete_organization_photo.js
+++ samples/organization/delete_organization_photo.js
@@ -1,19 +1,19 @@
 class DeleteOrganizationPhoto {
-    static async call() {
-        let environment = DataCenter.US.PRODUCTION();
-        let token = new OAuthBuilder()
-            .clientId("1000.xxxxxxx")
-            .scope("ZohoCRM.users.ALL,ZohoCRM.bulk.read,ZohoCRM.bulk.ALL,ZohoCRM.settings.currencies.READ,ZohoCRM.settings.currencies.CREATE,ZohoCRM.settings.currencies.UPDATE,ZohoCRM.share.Leads.ALL,Aaaserver.profile.Read,ZohoCRM.modules.ALL,ZohoCRM.settings.ALL,ZohoCRM.org.ALL,profile.userphoto.READ,ZohoFiles.files.ALL,ZohoCRM.files.READ,ZohoCRM.files.CREATE,ZohoCRM.mass_update.Leads.UPDATE,ZohoCRM.mass_update.Leads.READ,ZohoCRM.settings.currencies.read,ZohoCRM.settings.currencies.create,ZohoCRM.settings.currencies.update,ZohoCRM.coql.READ,ZohoCRM.notifications.ALL,ZohoCRM.templates.email.READ,ZohoCRM.templates.inventory.READ,ZohoCRM.settings.pipeline.ALL,ZohoCRM.send_mail.all.CREATE,ZohoCRM.settings.wizards.READ,ZohoCRM.settings.pipeline.READ,ZohoSearch.securesearch.READ,ZohoCRM.settings.pipeline.ALL,ZohoCRM.functions.execute.READ,ZohoCRM.functions.execute.CREATE,ZohoCRM.users.ALL,ZohoCRM.bulk.read,ZohoCRM.bulk.ALL,ZohoCRM.settings.currencies.READ,ZohoCRM.settings.currencies.CREATE,ZohoCRM.settings.currencies.UPDATE,ZohoCRM.share.Leads.ALL,Aaaserver.profile.Read,ZohoCRM.modules.ALL,ZohoCRM.settings.ALL,ZohoCRM.org.ALL,profile.userphoto.READ,ZohoFiles.files.ALL,ZohoCRM.files.READ,ZohoCRM.files.CREATE,ZohoCRM.mass_update.Leads.UPDATE,ZohoCRM.mass_update.Leads.READ,ZohoCRM.settings.currencies.read,ZohoCRM.settings.currencies.create,ZohoCRM.settings.currencies.update,ZohoCRM.coql.READ,ZohoCRM.notifications.ALL,ZohoCRM.templates.email.READ,ZohoCRM.templates.inventory.READ,ZohoCRM.settings.pipeline.ALL,ZohoCRM.send_mail.all.CREATE,ZohoCRM.settings.wizards.READ,ZohoCRM.settings.pipeline.READ,ZohoSearch.securesearch.READ,ZohoCRM.settings.pipeline.ALL,ZohoCRM.functions.execute.READ,ZohoCRM.functions.execute.CREATE")
-            .redirectURL("http://127.0.0.1:5500/redirect.html")
-            .build();
-        (await new InitializeBuilder())
-            .environment(environment)
-            .token(token)
-            .initialize();
-        await DeleteOrganizationPhoto.deleteOrganizationPhoto();
-    }
+	static async call() {
+		let environment = DataCenter.US.PRODUCTION();
+		let token = new OAuthBuilder()
+			.clientId("1000.xxxxxxx")
+			.scope("ZohoCRM.users.ALL,ZohoCRM.bulk.read,ZohoCRM.bulk.ALL,ZohoCRM.settings.currencies.READ,ZohoCRM.settings.currencies.CREATE,ZohoCRM.settings.currencies.UPDATE,ZohoCRM.share.Leads.ALL,Aaaserver.profile.Read,ZohoCRM.modules.ALL,ZohoCRM.settings.ALL,ZohoCRM.org.ALL,profile.userphoto.READ,ZohoFiles.files.ALL,ZohoCRM.files.READ,ZohoCRM.files.CREATE,ZohoCRM.mass_update.Leads.UPDATE,ZohoCRM.mass_update.Leads.READ,ZohoCRM.settings.currencies.read,ZohoCRM.settings.currencies.create,ZohoCRM.settings.currencies.update,ZohoCRM.coql.READ,ZohoCRM.notifications.ALL,ZohoCRM.templates.email.READ,ZohoCRM.templates.inventory.READ,ZohoCRM.settings.pipeline.ALL,ZohoCRM.send_mail.all.CREATE,ZohoCRM.settings.wizards.READ,ZohoCRM.settings.pipeline.READ,ZohoSearch.securesearch.READ,ZohoCRM.settings.pipeline.ALL,ZohoCRM.functions.execute.READ,ZohoCRM.functions.execute.CREATE,ZohoCRM.users.ALL,ZohoCRM.bulk.read,ZohoCRM.bulk.ALL,ZohoCRM.settings.currencies.READ,ZohoCRM.settings.currencies.CREATE,ZohoCRM.settings.currencies.UPDATE,ZohoCRM.share.Leads.ALL,Aaaserver.profile.Read,ZohoCRM.modules.ALL,ZohoCRM.settings.ALL,ZohoCRM.org.ALL,profile.userphoto.READ,ZohoFiles.files.ALL,ZohoCRM.files.READ,ZohoCRM.files.CREATE,ZohoCRM.mass_update.Leads.UPDATE,ZohoCRM.mass_update.Leads.READ,ZohoCRM.settings.currencies.read,ZohoCRM.settings.currencies.create,ZohoCRM.settings.currencies.update,ZohoCRM.coql.READ,ZohoCRM.notifications.ALL,ZohoCRM.templates.email.READ,ZohoCRM.templates.inventory.READ,ZohoCRM.settings.pipeline.ALL,ZohoCRM.send_mail.all.CREATE,ZohoCRM.settings.wizards.READ,ZohoCRM.settings.pipeline.READ,ZohoSearch.securesearch.READ,ZohoCRM.settings.pipeline.ALL,ZohoCRM.functions.execute.READ,ZohoCRM.functions.execute.CREATE")
+			.redirectURL("http://127.0.0.1:5500/redirect.html")
+			.build();
+		(await new InitializeBuilder())
+			.environment(environment)
+			.token(token)
+			.initialize();
+		await DeleteOrganizationPhoto.deleteOrganizationPhoto();
+	}
 
-    static async deleteOrganizationPhoto() {
+	static async deleteOrganizationPhoto() {
 		let orgOperations = new ZCRM.Org.Operations();
 		let response = await orgOperations.deleteOrganizationPhoto();
 		if (response != null) {
@@ -22,27 +22,27 @@ class DeleteOrganizationPhoto {
 			if (responseObject != null) {
 				if (responseObject instanceof ZCRM.Org.Model.SuccessResponse) {
 					console.log("Status: " + responseObject.getStatus().getValue());
-                    console.log("Code: " + responseObject.getCode().getValue());
-                    console.log("Details");
-                    let details = responseObject.getDetails();
-                    if (details != null) {
-                        Array.from(details.keys()).forEach(key => {
-                            console.log(key + ": " + details.get(key));
-                        });
-                    }
-                    console.log("Message: " + responseObject.getMessage());
+					console.log("Code: " + responseObject.getCode().getValue());
+					console.log("Details");
+					let details = responseObject.getDetails();
+					if (details != null) {
+						Array.from(details.keys()).forEach(key => {
+							console.log(key + ": " + details.get(key));
+						});
+					}
+					console.log("Message: " + responseObject.getMessage());
 				}
 				else if (responseObject instanceof ZCRM.Org.Model.APIException) {
 					console.log("Status: " + responseObject.getStatus().getValue());
-                    console.log("Code: " + responseObject.getCode().getValue());
-                    console.log("Details");
-                    let details = responseObject.getDetails();
-                    if (details != null) {
-                        Array.from(details.keys()).forEach(key => {
-                            console.log(key + ": " + details.get(key));
-                        });
-                    }
-                    console.log("Message: " + responseObject.getMessage());
+					console.log("Code: " + responseObject.getCode().getValue());
+					console.log("Details");
+					let details = responseObject.getDetails();
+					if (details != null) {
+						Array.from(details.keys()).forEach(key => {
+							console.log(key + ": " + details.get(key));
+						});
+					}
+					console.log("Message: " + responseObject.getMessage());
 				}
 			}
 		}
